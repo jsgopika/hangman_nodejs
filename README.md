fd9ef5ea24e235d350bdd09d4fba384777dbe5ed
Gopika <gopikajs1994@gmail.com>
2020-04-13 01:04:27 +0530

    game logic implemeted

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# hangman_nodejs
+# Components
+## Web Page -  FrontEnd
+Runs in browser, exchange infromation to the backend systsem using REST requests.
+
+## Server
+Servers the game logic. REST handler. 
+
+# Working
+If a user click start game -  backend will get info that , new game has created.
+
+on enterning each text, browser will send update to the server. server validate the request. Sends response.
---
+++ index.js
@@ -0,0 +1,75 @@
+const express =  require('express')
+const app = express()
+
+app.use(express.urlencoded({extended:true}))
+
+app.use(express.json())
+// Page Server
+app.get('/',function(req,res,next){
+  res.sendFile('index.html',{root:'./pages'})
+})
+
+// Polyfils
+const replaceCharAt = (x,i,char)=>x.substring(0, i) + char + x.substring(i+1);
+
+// Game related
+const game_const = {
+  secret:'nandu',
+  allowdAttempts:10,
+}
+
+const game_value = {
+  tries : 0,
+  maskedWord:''
+}
+
+function start_game(){
+  let { secret } = game_const
+  game_value.tries = 0
+  game_value.maskedWord =''
+  for(let i =0 ;i<secret.length;i++)
+    game_value.maskedWord+='*'
+}
+
+
+app.get('/new_game',(req,res,next)=>{
+  start_game()
+  const response = {
+    status:true,
+    game_value,
+    maxTries:game_const.allowdAttempts,
+  }
+  res.json(response)
+})
+
+app.post('/input',(req,res,next)=>{
+  const {char} =  req.body
+  
+  isCorrect = false
+  for(let i = 0;i < game_const.secret.length;i++){
+    const c = char[0]
+    if(game_const.secret[i] === c){
+      game_value.maskedWord= replaceCharAt(game_value.maskedWord,i,c)
+      isCorrect = true
+    }
+  }
+
+  if(!isCorrect){
+    game_value.tries++
+  }
+
+
+  const response = {
+    maskedWord : game_value.maskedWord,
+    completed: game_const.secret===game_value.maskedWord || game_value.tries >= game_const.allowdAttempts,
+    passed: game_const.secret === game_value.maskedWord,
+    remaining : game_const.allowdAttempts - game_value.tries
+  }
+
+  
+
+  res.json({status:true,response})
+})
+
+
+app.listen(8000)
